f799a3b67ea9c855779f4de104123842010c34cd
Michele Murphy <mmichelemurphy@gmail.com>
2014-05-08 21:50:02 -0700

    added user stories

---
+++ week_3/6_group_project/gc1_user_stories.js
@@ -0,0 +1,12 @@
+
+// USER STORIES
+
+// I want to have two groups of numbers: 
+// 	a. one group called oddLengthArray containing 7 numbers (using values 1-7)
+// 	b. one group called evenLengthArray containing 8 numbers (using values 4, 4, 5, 5, 6, 6, 6 and 7)
+
+// And I want a way to know each of the following:
+// 	a. the total of each group (call/name the way of knowing this: sum)
+// 	b. the average of each group (call/name it: mean)
+// 	c. the middle-most number for each group (call/name it: median)
+
