64fcedc274ae02089d50993fdafaa7764b628e35
Michele Murphy <mmichelemurphy@gmail.com>
2014-05-08 22:21:32 -0700

    changed group names

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -2,8 +2,8 @@
 // USER STORIES
 
 // I want to have two groups of numbers: 
-// 	a. one group called oddLengthArray containing 7 numbers (using values 1-7)
-// 	b. one group called evenLengthArray containing 8 numbers (using values 4, 4, 5, 5, 6, 6, 6 and 7)
+// 	a. one group containing 7 numbers (using values 1-7)
+// 	b. one group containing 8 numbers (using values 4, 4, 5, 5, 6, 6, 6 and 7)
 
 // And I want a way to know each of the following:
 // 	a. the total of each group (call/name the way of knowing this: sum)
