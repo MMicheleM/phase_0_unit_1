d7b73d35727b937c61bd3002a4413baa39d2f928
Michele Murphy <mmichelemurphy@gmail.com>
2014-05-11 20:09:05 -0700

    added reflection

--- week_3/6_group_project/gc1_user_stories.js
+++ week_3/6_group_project/gc1_user_stories.js
@@ -10,3 +10,31 @@
 // 	b. the average of each group (call/name it: mean)
 // 	c. the middle-most number for each group (call/name it: median)
 
+
+
+
+// * What parts of your strategy worked? What problems did you face?    
+	// Hard to ansswer this one as I had no real strategy other than
+	// "Get it done." There was some initial confusion in that the person before
+	// me had actually done more of a pseduo-code than a user story. Which
+	// was only a minor problem until I checked in with them and we swapped roles.
+
+// * What questions did you have while coding? What resources did you find to help you answer them?
+	//No questions per se "while coding" but understanding how to read tests and research on
+	//assert wree part of my process. Googling for assert and reading Mozilla's article on it helped.
+
+// * What concepts are you having trouble with, or did you just figure something out? If so, what?  
+	//None really. The story telling was a stretch in trying to keep it in true layperson terms but
+	// otherwise it was fun.
+
+// * Did you learn any new skills or tricks?
+	//I would say I now have working knowledge of TDD and of writing a story.
+
+// * How confident are you with each of the Learning Competencies? 
+	// To the degree that they were a part of the work I did I feel solid.
+	
+// * Which parts of the challenge did you enjoy?
+	// Learning about tests, how to read them and turn them into user stories.
+
+// * Which parts of the challenge did you find tedious?
+	// None really.
